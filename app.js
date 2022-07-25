8442d0417f98352a049a504fff8cba00a35141e4
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-07-25 23:49:12 +0100

    Creating a notes app with node

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+const chalk = require("chalk");
 const fs = require("fs");
+const { argv } = require("process");
 const getNotes = require("./notes");
 
-fs.writeFileSync("note.txt", "My name is Gabriel");
+//fs.writeFileSync("note.txt", "My name is Gabriel");
 
-fs.appendFileSync("note.txt", "\nI am 23 years old");
+//fs.appendFileSync("note.txt", "\nI am 23 years old");
 
-const msg = getNotes("gabby");
-console.log(msg);
+// const msg = getNotes("gabby");
+// console.log(msg);
+
+// console.log(chalk.bold.blue("Hello World"));
+//console.log(process.argv[2]);
+const command = process.argv[2];
+command === "add"
+  ? console.log("adding notes")
+  : command === "remove"
+  ? console.log("Removing notes")
+  : console.log("enter your command");
