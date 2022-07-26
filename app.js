4d938b53c116d762dc33c8a4e9e5a336b1c632c7
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-07-26 13:07:53 +0100

    Creating a cli with yargs npm package

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
 const chalk = require("chalk");
 const fs = require("fs");
 const { argv } = require("process");
 const getNotes = require("./notes");
+const { demandCommand, describe } = require("yargs");
 
 //fs.writeFileSync("note.txt", "My name is Gabriel");
 
@@ -12,9 +15,56 @@ const getNotes = require("./notes");
 
 // console.log(chalk.bold.blue("Hello World"));
 //console.log(process.argv[2]);
-const command = process.argv[2];
-command === "add"
-  ? console.log("adding notes")
-  : command === "remove"
-  ? console.log("Removing notes")
-  : console.log("enter your command");
+// const command = process.argv[2];
+// command === "add"
+//   ? console.log("adding notes")
+//   : command === "remove"
+//   ? console.log("Removing notes")
+//   : console.log("enter your command");
+//console.log(process.argv);
+
+//add , remove, read, and list
+
+//Create Add command
+// yargs.command("add", "add a new notes", () => {
+//   console.log("kkd");
+// }).argv;
+
+// yargs
+//   .command("remove", "remove a note", () => console.log("Removing notes"))
+//   .parse();
+// yargs.command("del", "remove ", () => console.log("Removing notes")).parse();
+
+yargs(hideBin(process.argv))
+  .command(
+    "add",
+    "add a new notes",
+    {
+      title: { describe: "Note title", demandOption: true, type: "string" },
+      body: {
+        describe: "Note body",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    (argv) => {
+      console.log(`Title: ${argv.title} \nBody: ${argv.body}\n`, argv);
+    }
+  )
+  .parse();
+
+// yargs(hideBin(process.argv))
+//   .command("remove", "Remove a note", () => {
+//     console.log("Removing a note");
+//   })
+//   .parse();
+
+// yargs(hideBin(process.argv)).command("list", "List your note", () => {
+//   console.log("Listing out all notes");
+// });
+// demandCommand(1).parse();
+
+// yargs(hideBin(process.argv)).command("read", "Read your note", () => {
+//   console.log("Reading a note");
+// });
+// demandCommand(1).parse();
